9e6caae215f93b0ef79b2a0484b1fad3afe62f49
ravi <rvvtici@gmail.com>
2026-06-18 09:38:32 -0300

    css sidenav & home

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,39 @@
-import Image from "next/image";
 import SideNav from "./sidenav";
 
 export default function Home() {
-  return (     
-    <div className="flex-grow h-screen flex-col md:flex-row md:overflow-y-auto">
-      <div className="w-full flex-none md:w-64">
-        <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-          <div className="w-full flex-none md:w-64">
-            <SideNav />
+  return (
+    <div className="flex h-screen flex-col dark:bg-black md:flex-row">
+      <SideNav />
+      {/*
+      <div className="flex flex-1 flex-col items-center justify-center overflow-y-auto bg-zinc-50 font-sans dark:bg-black">
+      */}
+        <main className="flex flex-row items-center px-16 py-16 gap-6 sm:items-start">
+          {/*<div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          */}
+          <div>
+
+            <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+              Ravi Macedo Ticianelli Lima
+            </h1>
+            <p className="text-sm max-w-md text-lg leading-8">
+              <a
+                href="https://github.com/rvvtici"
+                className=""
+                >
+                [ github ]
+              </a>{" "}
+              <a
+                href="https://linkedin.com/in/rvvtici"
+                className=""
+                >
+                [ linkedin ]
+              </a>
+            </p>
           </div>
-            <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-              <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-                <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-                  Ravi Macedo Ticianelli Lima
-                </h1>
-                  <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-                  <a
-                    href="https://github.com/rvvtici"
-                    className="font-medium text-zinc-950 dark:text-zinc-50"
-                  >
-                    [ github ] 
-                  </a>{" "}
-                  
-                  <a
-                    href="https://linkedin.com/in/rvvtici"
-                    className="font-medium text-zinc-950 dark:text-zinc-50"
-                  >
-                    [ linkedin ]
-                  </a>{" "}
-                </p>
-              </div> 
-            </main>
-          </div>
-        </div>
-      </div>
+          {/*</div>*/}
+        </main>
+      {/*</div>*/}
+    </div>
   );
 }
+
--- app/sidenav.tsx
+++ app/sidenav.tsx
@@ -2,10 +2,17 @@ import Link from 'next/link';
 
 export default function SideNav() {
   return (
-    <div className="flex h-full flex-col px-3 py-4 md:px-2">
-      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
-        <p>sidenav</p>
-      </div>
-    </div>
+    //<div className="flex h-full flex-col px-3 py-4 md:px-2">
+      <div className="flex dark:bg-black border-b-2
+      flex-row justify-center items-center space-x-2 py-1.5 w-full
+      md:flex-col md:space-x-0 md:space-y-2 md:py-10 md:w-60
+      md:justify-start
+      border-r-2
+      ">
+        
+          <p>sidenav</p>
+          <p>teste</p>
+        </div>
+    //</div>
   );
 }
